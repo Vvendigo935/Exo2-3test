0479e2aef78cd489f0121d3cb40ba77aa27e40b0
vivien <vivien@m2i.fr>
2025-06-27 16:32:50 +0200

    exo 2 et 3 test

--- Exo2test/src/test/java/example/exo2/FibTEST.java
+++ Exo2test/src/test/java/example/exo2/FibTEST.java
@@ -60,7 +60,7 @@ private Fib fib;
         List<Integer> result = fib.getFibSeries();
 
         // assert
-        Assert.assertFalse(result.contains(6));
+        Assert.assertTrue(result.size() == 6);
     }
 
     @Test
@@ -73,7 +73,37 @@ private Fib fib;
         List<Integer> result = fib.getFibSeries();
 
         // assert
-        Assert.assertFalse(4 == result.size());
+        Assert.assertFalse(result.contains(4));
+    }
+
+
+
+    @Test
+    public void whenResultHasListThatContains_0_1_1_2_3_5() {
+        // arrange
+        fib = new Fib(6);
+
+
+        // act
+        List<Integer> result = fib.getFibSeries();
+
+
+        // assert
+        Assert.assertEquals(List.of(0, 1, 1, 2, 3, 5), result);
+    }
+
+    @Test
+    public void whenResultIsSortedAscending() {
+        // arrange
+        fib = new Fib(6);
+
+
+        // act
+        List<Integer> result = fib.getFibSeries();
+
+
+        // assert
+        Assert.assertSame(List.of(0, 1, 2, 3, 4, 5), result);
     }
 
 
--- Exo2test/src/test/java/example/exo3/DiceScoreTEST.java
+++ Exo2test/src/test/java/example/exo3/DiceScoreTEST.java
@@ -1,10 +1,30 @@
 package example.exo3;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class DiceScoreTEST {
 
+    private DiceScore diceScore;
 
+    private InterfaceDice interfaceDice = Mockito.mock(InterfaceDice.class);
 
+    @Test
+    public void whenBothDiceScoreAreEqual() {
+        // arrange
+        diceScore = new DiceScore();
+        Mockito.when(diceScore.getScore()thenReturn(6));
 
+        // act
+        boolean result = diceScore.getScore().scoreFirst.equals(scoreSecond);
 
+        // assert
+        Assert.assertTrue(result);
+
+    }
 
 }
